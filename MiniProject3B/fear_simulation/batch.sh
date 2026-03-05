6694da934a4af52e8b0a928ca7cf7e5214d4219f
Caroline Lewis <47carolines@gmail.com>
2026-03-05 14:58:10 -0600

    updates to batch.sh for node2

--- MiniProject3B/fear_simulation/batch.sh
+++ MiniProject3B/fear_simulation/batch.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
-  
+#!/bin/bash
+
 #SBATCH --job-name=sim
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH --ntasks-per-node=4
+#SBATCH -n 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=worker
+#SBATCH --output=slurm_output_%j.txt
+#SBATCH --chdir=/home/ubuntu/CI-BioEng-Class/fear_simulation
 
 START=$(date)
-mpiexec nrniv -mpi -quiet -python run_bionet.py config.json
-END=$(date)
 
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate fear_sim
+
+srun python run_bionet.py config.json
+
+END=$(date)
 printf "Start: $START \nEnd:   $END\n"
