b4c91f9c56827d043d0d90506eaff26e93cd200e
Caroline Lewis <47carolines@gmail.com>
2026-03-15 20:24:28 -0500

    issues with check output

--- MiniProject4/fear_simulation/check_output.py
+++ MiniProject4/fear_simulation/check_output.py
@@ -1,13 +1,16 @@
+import os
 import numpy as np
 from bmtk.utils.reports.spike_trains import SpikeTrains
 
-spikes = SpikeTrains.load(
-    'output/spikes.h5',
-    population='PING-Assembly'
-)
+spikes_file = os.path.join(os.path.dirname(__file__), 'output', 'spikes.h5')
 
-ISI = np.diff(np.sort(spikes.get_times(node_id=0)))
+spikes = SpikeTrains.load(spikes_file, population='PING-Assembly')
+times = np.sort(spikes.get_times(node_id=0))
 
-firing_rate = 1000 / np.mean(ISI)
+if len(times) < 2:
+    firing_rate = 0.0
+else:
+    ISI = np.diff(times)
+    firing_rate = 1000 / np.mean(ISI)
 
 print(firing_rate)
